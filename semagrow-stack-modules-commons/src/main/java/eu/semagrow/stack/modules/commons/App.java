85246fb1f8d68c75b994268cc23f6347ffa8ea7c
Jürgen Jakobitsch <turnguard@linux-1rgw.site>
2013-09-03 00:16:10 +0200

    base modules

---
+++ semagrow-stack-modules-commons/src/main/java/eu/semagrow/stack/modules/commons/App.java
@@ -0,0 +1,13 @@
+package eu.semagrow.stack.modules.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
