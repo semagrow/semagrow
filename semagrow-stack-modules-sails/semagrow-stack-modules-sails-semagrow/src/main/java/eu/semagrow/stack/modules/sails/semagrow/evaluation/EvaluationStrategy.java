05db9ed020718a4874c86c30a4e3eb0f1062f980
jürgen jakobitsch <turnguard@linux-nb59.site>
2014-04-06 22:40:06 +0200

    SemaGrowSail

---
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/EvaluationStrategy.java
@@ -0,0 +1,55 @@
+package eu.semagrow.stack.modules.sails.semagrow.evaluation;
+
+import info.aduna.iteration.CloseableIteration;
+import org.openrdf.model.*;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.evaluation.TripleSource;
+import org.openrdf.query.algebra.evaluation.impl.EvaluationStrategyImpl;
+
+/**
+ * Created by angel on 3/12/14.
+ */
+public class EvaluationStrategy extends EvaluationStrategyImpl {
+
+    /*
+    public EvaluationStrategy(FederatedServiceResolver serviceResolver)
+    {
+        super(new TripleSource() {
+            public CloseableIteration<? extends Statement, QueryEvaluationException>
+                    getStatements(Resource resource, URI uri, Value value, Resource... resources) throws QueryEvaluationException {
+                return null;
+            }
+
+            public ValueFactory getValueFactory() {
+                return ValueFactoryImpl.getInstance();
+            }
+        }, serviceResolver);
+
+    }
+    */
+
+    public EvaluationStrategy()
+    {
+        super(new TripleSource() {
+            public CloseableIteration<? extends Statement, QueryEvaluationException>
+            getStatements(Resource resource, URI uri, Value value, Resource... resources) throws QueryEvaluationException {
+                return null;
+            }
+
+            public ValueFactory getValueFactory() {
+                return ValueFactoryImpl.getInstance();
+            }
+        });
+    }
+
+    @Override
+    public CloseableIteration<BindingSet, QueryEvaluationException>
+            evaluate(TupleExpr expr, BindingSet bindings) throws QueryEvaluationException {
+
+        return super.evaluate(expr, bindings);
+    }
+
+}
---
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/iterator/RemoteQueryIteration.java
@@ -0,0 +1,7 @@
+package eu.semagrow.stack.modules.sails.semagrow.evaluation.iterator;
+
+/**
+ * Created by angel on 3/13/14.
+ */
+public class RemoteQueryIteration {
+}
