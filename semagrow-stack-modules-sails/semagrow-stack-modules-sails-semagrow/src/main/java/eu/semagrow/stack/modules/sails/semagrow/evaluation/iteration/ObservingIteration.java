3ffe011fd08b9b4e2470b2f413b5b8d4abd9249c
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-06-16 13:16:04 +0300

    observing and rate iteration

--- semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/iteration/ObservingIteration.java
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/iteration/ObservingIteration.java
@@ -6,24 +6,24 @@ import info.aduna.iteration.IterationWrapper;
 /**
  * Created by angel on 6/12/14.
  */
-public abstract class RecordingIteration<E,X extends Exception> extends IterationWrapper<E,X> {
+public abstract class ObservingIteration<E,X extends Exception> extends IterationWrapper<E,X> {
 
-    public RecordingIteration(Iteration<E,X> iter) {
+    public ObservingIteration(Iteration<E,X> iter) {
         super(iter);
     }
 
-    public abstract void record(E e);
+    public abstract void observe(final E e);
 
-    public abstract void record(X x);
+    public abstract void observeExceptionally(final X x);
 
     @Override
     public E next() throws X {
         try {
             E item = super.next();
-            record(item);
+            observe(item);
             return item;
         } catch (Exception exception) {
-            record((X)exception);
+            observeExceptionally((X)exception);
             throw (X)exception;
         }
     }
