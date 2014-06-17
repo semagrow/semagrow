df6db9508bca027b5774a5421ffbde11f9b72994
jürgen jakobitsch <turnguard@linux-nb59.site>
2014-06-17 18:51:37 +0200

    sparqlSamples predicates and class

--- semagrow-stack-modules-vocabulary/src/main/java/eu/semagrow/stack/modules/vocabulary/SEMAGROW.java
+++ semagrow-stack-modules-vocabulary/src/main/java/eu/semagrow/stack/modules/vocabulary/SEMAGROW.java
@@ -11,6 +11,26 @@ import org.openrdf.model.impl.ValueFactoryImpl;
 public final class SEMAGROW {
     public static final String NAMESPACE = "http://schema.semagrow.eu/";    
     
+    public static final class SYSTEM {
+        
+        public static final String NAMESPACE = SEMAGROW.NAMESPACE+"system/1.0.0/";
+        
+        public static final class SPARQL_SAMPLES {
+            public static final String NAMESPACE = SEMAGROW.SYSTEM.NAMESPACE+"samples/";
+            /* Classes */
+            public static final URI SPARQL_SAMPLE;
+            /* Predicates */
+            
+            /*  http://schema.semagrow.eu/system/1.0.0/samples/sparqlSampleText */
+            public static final URI SPARQL_SAMPLE_TEXT;
+            
+            static {
+                SPARQL_SAMPLE = ValueFactoryImpl.getInstance().createURI(SEMAGROW.SYSTEM.SPARQL_SAMPLES.NAMESPACE, "SparqlSample");
+                SPARQL_SAMPLE_TEXT = ValueFactoryImpl.getInstance().createURI(SEMAGROW.SYSTEM.SPARQL_SAMPLES.NAMESPACE, "sparqlSampleText"); 
+            }
+        }
+    }
+    
     public static final class SAILS {
         public static final String NAMESPACE = SEMAGROW.NAMESPACE+"sails/1.0.0/";        
         
