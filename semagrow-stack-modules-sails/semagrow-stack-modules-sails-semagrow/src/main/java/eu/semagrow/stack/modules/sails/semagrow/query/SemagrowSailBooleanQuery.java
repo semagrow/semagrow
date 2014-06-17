a5604a3da1c1e2451f2b03949cd7f2f1fc9598fe
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-06-17 12:56:05 +0300

    fix incompatible interfaces

--- semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/query/SemagrowSailBooleanQuery.java
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/query/SemagrowSailBooleanQuery.java
@@ -1,8 +1,11 @@
 package eu.semagrow.stack.modules.sails.semagrow.query;
 
+import eu.semagrow.stack.modules.api.decomposer.QueryDecompositionException;
 import eu.semagrow.stack.modules.api.query.SemagrowBooleanQuery;
+import eu.semagrow.stack.modules.sails.semagrow.SemagrowSailConnection;
 import eu.semagrow.stack.modules.sails.semagrow.SemagrowSailRepositoryConnection;
 import info.aduna.iteration.CloseableIteration;
+import org.openrdf.model.URI;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.Dataset;
 import org.openrdf.query.QueryEvaluationException;
@@ -12,15 +15,27 @@ import org.openrdf.repository.sail.SailBooleanQuery;
 import org.openrdf.sail.SailConnection;
 import org.openrdf.sail.SailException;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by angel on 6/13/14.
  */
-public class SemagrowSailBooleanQuery extends SemagrowSailQuery
+public class SemagrowSailBooleanQuery extends SailBooleanQuery
         implements SemagrowBooleanQuery {
+
+
+    private Set<URI> excludedSources;
+    private Set<URI> includeOnlySources;
+
 
     public SemagrowSailBooleanQuery(ParsedBooleanQuery tupleQuery,
                                     SemagrowSailRepositoryConnection sailConnection) {
         super(tupleQuery, sailConnection);
+
+        excludedSources = new HashSet<URI>();
+        includeOnlySources = new HashSet<URI>();
     }
 
     @Override
@@ -57,4 +72,26 @@ public class SemagrowSailBooleanQuery extends SemagrowSailQuery
             throw new QueryEvaluationException(e.getMessage(), e);
         }
     }
+
+    public TupleExpr getDecomposedQuery() throws QueryDecompositionException {
+
+        SemagrowSailConnection conn = (SemagrowSailConnection) getConnection().getSailConnection();
+        TupleExpr initialExpr = getParsedQuery().getTupleExpr();
+        TupleExpr expr = initialExpr.clone();
+        Dataset dataset = getDataset();
+
+        if (dataset == null) {
+            // No external dataset specified, use query's own dataset (if any)
+            dataset = getParsedQuery().getDataset();
+        }
+        return conn.decompose(expr, dataset, getBindings());
+    }
+
+    public void addExcludedSource(URI source) { excludedSources.add(source); }
+
+    public void addIncludedSource(URI source) { includeOnlySources.add(source); }
+
+    public Collection<URI> getExcludedSources() { return excludedSources; }
+
+    public Collection<URI> getIncludedSources() { return includeOnlySources; }
 }
--- semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/query/SemagrowSailTupleQuery.java
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/query/SemagrowSailTupleQuery.java
@@ -1,9 +1,11 @@
 package eu.semagrow.stack.modules.sails.semagrow.query;
 
+import eu.semagrow.stack.modules.api.decomposer.QueryDecompositionException;
 import eu.semagrow.stack.modules.api.query.SemagrowTupleQuery;
 import eu.semagrow.stack.modules.sails.semagrow.SemagrowSailRepositoryConnection;
 import eu.semagrow.stack.modules.sails.semagrow.SemagrowSailConnection;
 import info.aduna.iteration.CloseableIteration;
+import org.openrdf.model.URI;
 import org.openrdf.query.*;
 import org.openrdf.query.algebra.TupleExpr;
 import org.openrdf.query.impl.TupleQueryResultImpl;
@@ -13,17 +15,26 @@ import org.openrdf.repository.sail.SailTupleQuery;
 import org.openrdf.sail.SailException;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by angel on 6/9/14.
  */
-public class SemagrowSailTupleQuery extends SemagrowSailQuery implements SemagrowTupleQuery {
+public class SemagrowSailTupleQuery extends SailTupleQuery implements SemagrowTupleQuery {
 
     private boolean includeProvenanceData = false;
+
+    private Set<URI> excludedSources;
+    private Set<URI> includeOnlySources;
+
 
     public SemagrowSailTupleQuery(ParsedTupleQuery query, SemagrowSailRepositoryConnection connection)
     {
         super(query,connection);
+        excludedSources = new HashSet<URI>();
+        includeOnlySources = new HashSet<URI>();
     }
 
     public TupleQueryResult evaluate() throws QueryEvaluationException {
@@ -47,13 +58,6 @@ public class SemagrowSailTupleQuery extends SemagrowSailQuery implements Semagro
         }
     }
 
-    public void evaluate(TupleQueryResultHandler handler)
-            throws QueryEvaluationException, TupleQueryResultHandlerException {
-
-        TupleQueryResult queryResult = evaluate();
-        QueryResults.report(queryResult, handler);
-    }
-
     public void setIncludeProvenanceData(boolean includeProvenance) {
         includeProvenanceData = includeProvenance;
     }
@@ -64,4 +68,26 @@ public class SemagrowSailTupleQuery extends SemagrowSailQuery implements Semagro
     public SemagrowSailRepositoryConnection getConnection() {
         return (SemagrowSailRepositoryConnection) super.getConnection();
     }
+
+    public TupleExpr getDecomposedQuery() throws QueryDecompositionException {
+
+        SemagrowSailConnection conn = (SemagrowSailConnection) getConnection().getSailConnection();
+        TupleExpr initialExpr = getParsedQuery().getTupleExpr();
+        TupleExpr expr = initialExpr.clone();
+        Dataset dataset = getDataset();
+
+        if (dataset == null) {
+            // No external dataset specified, use query's own dataset (if any)
+            dataset = getParsedQuery().getDataset();
+        }
+        return conn.decompose(expr, dataset, getBindings());
+    }
+
+    public void addExcludedSource(URI source) { excludedSources.add(source); }
+
+    public void addIncludedSource(URI source) { includeOnlySources.add(source); }
+
+    public Collection<URI> getExcludedSources() { return excludedSources; }
+
+    public Collection<URI> getIncludedSources() { return includeOnlySources; }
 }
