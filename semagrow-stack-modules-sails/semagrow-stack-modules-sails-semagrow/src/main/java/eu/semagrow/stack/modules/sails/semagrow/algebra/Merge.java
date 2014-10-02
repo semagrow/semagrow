7c3156d0241993aac5651fd06cbc27f38e8788ba
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-10-02 16:23:00 +0300

    merge and merge join operators

---
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/algebra/Merge.java
@@ -0,0 +1,28 @@
+package eu.semagrow.stack.modules.sails.semagrow.algebra;
+
+import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.Union;
+
+/**
+ * Created by angel on 10/2/14.
+ */
+public class Merge extends Union {
+
+    public Merge(TupleExpr leftArg, TupleExpr rightArg) {
+        super(leftArg, rightArg);
+    }
+
+    @Override
+    public int hashCode() {
+        return "hash".hashCode() + super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Merge) {
+            Merge j = (Merge) o;
+            return getLeftArg().equals(j.getLeftArg()) && getRightArg().equals(j.getRightArg());
+        }
+        return false;
+    }
+}
