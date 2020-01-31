89ee02022bc94f62fee84ebefb4bb7c9e4b3d948
Nefeli Prokopaki Kostopoulou <nefelipk@gmail.com>
2020-01-31 14:14:30 +0200

    geospatial.algebra.strdf isEmpty

---
+++ geospatial/src/main/java/org/semagrow/geospatial/algebra/strdf/StIsEmpty.java
@@ -0,0 +1,41 @@
+package org.semagrow.geospatial.algebra.strdf;
+
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.GEO;
+import org.eclipse.rdf4j.query.algebra.evaluation.ValueExprEvaluationException;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
+import org.locationtech.spatial4j.shape.Shape;
+import org.semagrow.geospatial.commons.IllegalGeometryException;
+import org.semagrow.geospatial.commons.SimpleShapeFactory;
+import org.semagrow.geospatial.vocabulary.STRDF;
+
+public class StIsEmpty implements Function {
+    @Override
+    public String getURI() {
+        return STRDF.isEmpty.toString();
+    }
+
+    @Override
+    public Value evaluate(ValueFactory valueFactory, Value... values) throws ValueExprEvaluationException {
+        if (values.length != 1) {
+            throw new ValueExprEvaluationException("Wrong number of arguments");
+        }
+        if (!(values[0] instanceof Literal && ((Literal) values[0]).getDatatype().equals(GEO.WKT_LITERAL))) {
+            throw new ValueExprEvaluationException("Illegal argument format");
+        }
+
+        String wktString = values[0].stringValue();
+        Shape shape = null;
+        try {
+            shape = SimpleShapeFactory.getInstance().createShape(wktString);
+        } catch (IllegalGeometryException e) {
+            throw new ValueExprEvaluationException("Illegal WKT format", e);
+        }
+        boolean isEmpty = shape.isEmpty();
+        Value value = SimpleValueFactory.getInstance().createLiteral(isEmpty);
+        return  value;
+    }
+}
