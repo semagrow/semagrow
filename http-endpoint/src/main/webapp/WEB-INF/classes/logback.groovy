ca89abf224efef2c4802f3739621397f6901c9ec
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2020-04-22 19:08:16 +0300

    enable logback in tomcat bundle

---
+++ http-endpoint/src/main/webapp/WEB-INF/classes/logback.groovy
@@ -0,0 +1,27 @@
+
+appender( "PROCFLOW", ConsoleAppender ) {
+  encoder( PatternLayoutEncoder) {
+    pattern = "%.-1level - %date{ISO8601} - [%10.10thread] - %20.-20logger{0} - %.12X{uuid} - %X{nestingLevel} - %msg%n"
+  }
+}
+
+appender( "CONSOLE", ConsoleAppender ) {
+  encoder( PatternLayoutEncoder) {
+    pattern = "%.-1level - %date{ISO8601} - [%10.10thread] - %20.-20logger{0} - %msg%n"
+  }
+}
+
+
+logger( "org.semagrow", WARN , ["PROCFLOW"], false )
+logger( "org.semagrow.plan", INFO, ["PROCFLOW"], false)
+logger( "org.semagrow.estimator", WARN, ["PROCFLOW"], false)
+logger( "org.semagrow.evaluation", INFO, ["PROCFLOW"], false)
+logger( "org.semagrow.connector.sparql", INFO, ["PROCFLOW"], false)
+logger( "org.semagrow.connector.sparql.execution.TupleQueryResultPublisher", WARN, ["PROCFLOW"], false)
+logger( "org.semagrow.http", INFO, ["PROCFLOW"], false )
+logger( "org.semagrow.query", INFO, ["PROCFLOW"], false )
+logger( "org.semagrow.sail", INFO, ["PROCFLOW"], false )
+
+logger( "reactor", WARN, ["PROCFLOW"], false )
+
+root( INFO, ["CONSOLE"] )
